de8bbab21dfd9b8709d9686f55a097787c52fc76
elliottf@microsoft.com <elliottf@microsoft.com>
2020-07-03 20:39:53 -0400

    .

--- 1-Forest_1-DomainController/DSC/RESTARTVM.ps1
+++ 1-Forest_1-DomainController/DSC/RESTARTVM.ps1
@@ -6,16 +6,26 @@
 
     Node localhost
     {
+        File Machineconfig
+        {
+            Type = 'Directory'
+            DestinationPath = 'C:\MachineConfig'
+            Ensure = "Present"
+        }
+
         Script RestartVM
         {
             SetScript =
             {
                 # Restart VM
-                Restart-Computer -Force
+                $Path = Get-Item -Path C:\MachineConfig\RebootComplete.txt
+                IF ($Path -eq $Null) {Restart-Computer -Force}
 
+                Set-Content -Path C:\MachineConfig\RebootComplete.txt -Value "Complete"
             }
             GetScript =  { @{} }
             TestScript = { $false}
         }
+
     }
 }
