7037b7eccf8174c9af2f16a708801626f1ebef4b
elliottf@microsoft.com <elliottf@microsoft.com>
2020-07-03 20:11:43 -0400

    .

---
+++ 1-Forest_1-DomainController/DSC/RESTARTVM.ps1
@@ -0,0 +1,21 @@
+﻿configuration RESTARTVM
+{
+   param
+   (
+    )
+
+    Node localhost
+    {
+        Script RestartVM
+        {
+            SetScript =
+            {
+                # Restart VM
+                Restart-Computer -Force
+
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+        }
+    }
+}
