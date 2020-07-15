0d2972391e0809a300796226607682f41847c691
elliottf@microsoft.com <elliottf@microsoft.com>
2020-07-15 19:00:42 -0400

    .

--- Azure-Site-Recovery_Same-Address-Space/Scripts/createhsmkey.ps1
+++ Azure-Site-Recovery_Same-Address-Space/Scripts/createhsmkey.ps1
@@ -5,7 +5,7 @@
     [string] [Parameter(Mandatory=$true)] $KeyDestination,
     [string] [Parameter(Mandatory=$true)] $DESName,
     [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
-    [string] [Par.meter(Mandatory=$true)] $Location    
+    [string] [Parameter(Mandatory=$true)] $Location    
     )
 
 $KeyVault = Get-AzKeyVault -VaultName $vaultName
@@ -16,5 +16,5 @@ $Key = Add-AzKeyVaultKey -VaultName $KeyVault.VaultName -Name $Keyname -Expires 
 
 $desConfig = New-AzDiskEncryptionSetConfig -Location $Location -SourceVaultId $keyVault.ResourceId -KeyUrl $key.Key.Kid -IdentityType SystemAssigned
 
-$des = New-AzDiskEncryptionSet -Name $DESName -ResourceGroupName $ResourceGroupName -DiskEncryptionSet $desConfig
+$des = New-AzDiskEncryptionSet -Name $DESName -ResourceGroupName $ResourceGroupName -InputObject $desConfig
 Set-AzKeyVaultAccessPolicy -VaultName $keyVault.VaultName -ObjectId $des.Identity.PrincipalId -PermissionsToKeys wrapkey,unwrapkey,get
