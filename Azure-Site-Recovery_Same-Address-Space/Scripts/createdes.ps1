319a296e50792735f76768805dbcc76915da018c
elliottf@microsoft.com <elliottf@microsoft.com>
2020-07-16 14:32:25 -0400

    d

--- Azure-Site-Recovery_Same-Address-Space/Scripts/createdes.ps1
+++ Azure-Site-Recovery_Same-Address-Space/Scripts/createdes.ps1
@@ -12,4 +12,4 @@ $Key = Get-AzKeyVaultKey -VaultName $KeyVault.VaultName -Name $KeyName
 $desConfig = New-AzDiskEncryptionSetConfig -Location $Location -SourceVaultId $KeyVault.ResourceId -KeyUrl $key.Key.Kid -IdentityType SystemAssigned
 
 $des = New-AzDiskEncryptionSet -Name $DESName -ResourceGroupName $ResourceGroupName -InputObject $desConfig
-Set-AzKeyVaultAccessPolicy -VaultName $KeyVault.VaultName -ObjectId $des.Identity.PrincipalId -PermissionsToKeys wrapkey,unwrapkey,get
+# Set-AzKeyVaultAccessPolicy -VaultName $KeyVault.VaultName -ObjectId $des.Identity.PrincipalId -PermissionsToKeys wrapkey,unwrapkey,get
