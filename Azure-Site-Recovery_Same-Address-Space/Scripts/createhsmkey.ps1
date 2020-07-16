d84c67b0688aa12dcae7220e5d5079c02c0c6fc3
elliottf@microsoft.com <elliottf@microsoft.com>
2020-07-16 13:34:55 -0400

    .

--- Azure-Site-Recovery_Same-Address-Space/Scripts/createhsmkey.ps1
+++ Azure-Site-Recovery_Same-Address-Space/Scripts/createhsmkey.ps1
@@ -2,10 +2,7 @@
     [string] [Parameter(Mandatory=$true)] $vaultName,
     [string] [Parameter(Mandatory=$true)] $KeyName,
     [string] [Parameter(Mandatory=$true)] $KeySize,
-    [string] [Parameter(Mandatory=$true)] $KeyDestination,
-    [string] [Parameter(Mandatory=$true)] $DESName,
-    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
-    [string] [Parameter(Mandatory=$true)] $Location    
+    [string] [Parameter(Mandatory=$true)] $KeyDestination
     )
 
 $KeyVault = Get-AzKeyVault -VaultName $vaultName
@@ -13,8 +10,3 @@ $KeyExpires = (Get-Date).AddYears(2).ToUniversalTime()
 $KeyNotBefore = (Get-Date).ToUniversalTime()
 
 Add-AzKeyVaultKey -VaultName $KeyVault.VaultName -Name $Keyname -Expires $KeyExpires -NotBefore $KeyNotBefore -Size $KeySize -Destination $KeyDestination
-
-$desConfig = New-AzDiskEncryptionSetConfig -Location $Location -SourceVaultId $keyVault.ResourceId -KeyUrl $key.Key.Kid -IdentityType SystemAssigned
-
-$des = New-AzDiskEncryptionSet -Name $DESName -ResourceGroupName $ResourceGroupName -InputObject $desConfig
-Set-AzKeyVaultAccessPolicy -VaultName $keyVault.VaultName -ObjectId $des.Identity.PrincipalId -PermissionsToKeys wrapkey,unwrapkey,get
