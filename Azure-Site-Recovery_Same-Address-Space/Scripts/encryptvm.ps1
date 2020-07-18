f68374f94913e64c17853690e63cfca34a423362
elliottf@microsoft.com <elliottf@microsoft.com>
2020-07-18 08:45:21 -0400

    dfdf

--- Azure-Site-Recovery_Same-Address-Space/Scripts/encryptvm.ps1
+++ Azure-Site-Recovery_Same-Address-Space/Scripts/encryptvm.ps1
@@ -10,6 +10,6 @@ param(
     $OSDisk = Get-AzDisk -name $vm.StorageProfile.OsDisk.Name
     $DataDisk = Get-AzDisk -name $vm.StorageProfile.DataDisks[0].Name
     Stop-AzVm -Name $VMName -ResourceGroupName $ResourceGroupName -Force
-    IF ($OSDisk.Encryption.Type -ne $EncryptionType) {New-AzDiskUpdateConfig -EncryptionType $EncryptionKeyType -DiskEncryptionSetId $des.Id | Update-AzDisk -ResourceGroupName $ResourceGroupName -DiskName $OSDisk.Name}
-    IF ($DataDisk.Encryption.Type -ne $EncryptionType) {New-AzDiskUpdateConfig -EncryptionType $EncryptionKeyType -DiskEncryptionSetId $des.Id | Update-AzDisk -ResourceGroupName $ResourceGroupName -DiskName $DataDisk.Name}
+    IF ($OSDisk.Encryption.Type -ne $EncryptionType) {New-AzDiskUpdateConfig -EncryptionType $EncryptionType -DiskEncryptionSetId $des.Id | Update-AzDisk -ResourceGroupName $ResourceGroupName -DiskName $OSDisk.Name}
+    IF ($DataDisk.Encryption.Type -ne $EncryptionType) {New-AzDiskUpdateConfig -EncryptionType $EncryptionType -DiskEncryptionSetId $des.Id | Update-AzDisk -ResourceGroupName $ResourceGroupName -DiskName $DataDisk.Name}
     Start-AzVm -Name $VMName -ResourceGroupName $ResourceGroupName
