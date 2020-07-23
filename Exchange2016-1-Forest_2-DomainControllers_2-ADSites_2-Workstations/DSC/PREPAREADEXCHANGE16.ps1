153c28e975d09a9f7c03fd00717352982557d588
elliottf@microsoft.com <elliottf@microsoft.com>
2020-07-23 09:42:54 -0400

    .

---
+++ Exchange2016-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/PREPAREADEXCHANGE16.ps1
@@ -0,0 +1,39 @@
+﻿configuration PREPAREADEXCHANGE16
+{
+   param
+   (
+        [String]$ExchangeOrgName,
+        [String]$NetBiosDomain,
+        [String]$DC1Name,
+        [String]$DC2Name,
+        [String]$BaseDN,
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+    Import-DscResource -Module xPSDesiredStateConfiguration # Used for xRemoteFile
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        Script PrepareExchange2016AD
+        {
+            SetScript =
+            {
+                # Create Exchange AD Deployment
+                K:\Setup.exe /PrepareSchema /DomainController:"$using:dc1Name" /IAcceptExchangeServerLicenseTerms
+                K:\Setup.exe /PrepareAD /on:"$using:ExchangeOrgName" /DomainController:"$using:dc1Name" /IAcceptExchangeServerLicenseTerms
+
+                repadmin /replicate "$using:DC1name" "$using:DC2name" "$using:BaseDN"
+                repadmin /replicate "$using:DC2name" "$using:DC1name" "$using:BaseDN"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            PsDscRunAsCredential = $DomainCreds
+        }
+    }
+}
---
+++ Exchange2016-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/README.md
@@ -0,0 +1,8 @@
+# Exchange 2016, 1-Forest/Domain, 2-DomainControllers, 2-ADSites, 2-Workstation, 2-Tier PKI
+
+This Templates deploys a Single Forest/Domain, 2 Active Directory Sites with a Domain Controller in each, Organizational Units, DNS Records and 2 Domain Joined Windows 10 Workstations in each AD Site.  I additional to AD Exchange 2016 is deployed on 2 Windows 2016 Servers in each AD Site configured as DAG Members in a single DAG.
+
+Click the button below to deploy
+
+[![Deploy To Azure](https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/1-CONTRIBUTION-GUIDE/images/deploytoazure.svg?sanitize=true)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Felliottfieldsjr%2FKillerHomeLab%2Fmaster%2FExchange2016-1-Forest_2-DomainControllers_2-ADSites_2-Workstations%2Fazuredeploy.json)
+[![Deploy To Azure US Gov](https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/1-CONTRIBUTION-GUIDE/images/deploytoazuregov.svg?sanitize=true)](https://portal.azure.us/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Felliottfieldsjr%2FKillerHomeLab%2Fmaster%2FExchange2016-1-Forest_2-DomainControllers_2-ADSites_2-Workstations%2Fazuredeploy.json)
