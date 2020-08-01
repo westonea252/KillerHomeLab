55ab7299cfac2440c9ed2146f7a6f2fbe19e083a
elliottf@microsoft.com <elliottf@microsoft.com>
2020-08-01 09:45:30 -0400

    Updating AD Connect Download

---
+++ HybridIdentity-ADFS-WAP-ADConnect/DSC/DOWNLOADAADC.ps1
@@ -0,0 +1,35 @@
+Configuration DOWNLOADAADC
+{
+   param
+   (
+        [String]$AzureADConnectDownloadUrl,
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+ 
+    Import-DscResource -Module ComputerManagementDsc # Used for TimeZone
+    Import-DscResource -Module xPSDesiredStateConfiguration # Used for xRemote
+
+    Node localhost
+    {        
+        File ADConnectInstall
+        {
+            Type = 'Directory'
+            DestinationPath = 'C:\ADConnectInstall'
+            Ensure = "Present"
+        }
+
+        xRemoteFile DownloadAzureADConnect
+        {
+            DestinationPath = 'C:\ADConnectInstall\AzureADConnect.msi'
+            Uri             = "$AzureADConnectDownloadUrl"
+            UserAgent       = "[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer"
+            DependsOn =    "[File]ADConnectInstall"
+        }
+
+        TimeZone SetTimeZone
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = 'Eastern Standard Time'
+        }
+     }
+  }
