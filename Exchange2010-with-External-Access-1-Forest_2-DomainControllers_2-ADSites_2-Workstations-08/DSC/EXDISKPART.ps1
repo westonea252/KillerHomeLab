ac64afc195732bf520a5c51807f3538ff5f96c6c
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-08-15 16:04:04 -0400

    Adding Corrected Exchange2010 with2008

---
+++ Exchange2010-with-External-Access-1-Forest_2-DomainControllers_2-ADSites_2-Workstations-08/DSC/EXDISKPART.ps1
@@ -0,0 +1,38 @@
+﻿configuration EXDISKPART
+{
+   param
+   (
+    )
+
+    Import-DscResource -Module xPSDesiredStateConfiguration # Used for xRemoteFile
+
+    Node localhost
+    {
+        File DiskConfig
+        {
+            Type = 'Directory'
+            DestinationPath = 'C:\DiskConfig'
+            Ensure = "Present"
+        }
+
+        xRemoteFile DiskPart
+        {
+            DestinationPath = "C:\DiskConfig\ExchangeDiskPart.txt"
+            Uri             = "https://raw.githubusercontent.com/elliottfieldsjr/KillerHomeLab/master/Exchange2010-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/Scripts/ExchangeDiskpart.txt"
+            UserAgent       = "[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer"
+            DependsOn =    "[File]DiskConfig"
+        }
+
+        Script ConfigureDisks
+        {
+            SetScript =
+            {
+                # Partition Volumes
+                C:\Windows\System32\diskpart.exe /s C:\DiskConfig\ExchangeDiskPart.txt
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = '[xRemoteFile]DiskPart'
+        }
+    }
+}
