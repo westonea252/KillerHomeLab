100b4df3fbe47cc6f85c9cb1136b2fa60e57f331
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-08-24 15:07:58 -0400

    Update Deployments

---
+++ Exchange2019-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/EXCHANGE19.ps1
@@ -0,0 +1,40 @@
+﻿configuration EXCHANGE19
+{
+   param
+   (
+        [String]$NetBiosDomain,
+        [String]$DBName,
+        [String]$SetupDC,
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        File ExchangeInstall
+        {
+            Type = 'Directory'
+            DestinationPath = 'S:\ExchangeInstall'
+            Ensure = "Present"
+        }
+
+        Script InstallExchange2019
+        {
+            SetScript =
+            {
+                Set-Content -Path S:\ExchangeInstall\DeployExchange.cmd -Value "L:\Setup.exe /Iacceptexchangeserverlicenseterms /Mode:Install /Role:Mailbox /DbFilePath:M:\$using:DBName\$using:DBName.edb /LogFolderPath:M:\$using:DBName /MdbName:$using:DBName /dc:$using:SetupDC"
+                S:\ExchangeInstall\DeployExchange.cmd
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            PsDscRunAsCredential = $DomainCreds
+        }
+
+    }
+}
