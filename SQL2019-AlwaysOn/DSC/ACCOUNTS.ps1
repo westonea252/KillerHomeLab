143a19e63b9410c3a9c24e17d0d671ec8cfc0627
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-11-26 10:39:33 -0500

    Added Account Deployment

---
+++ SQL2019-AlwaysOn/DSC/ACCOUNTS.ps1
@@ -0,0 +1,56 @@
+﻿configuration ACCOUNTS
+{
+   param
+   (
+        [String]$DomainName,        
+        [String]$BaseDN,
+        [String]$NetBiosDomain,
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node localhost
+    {
+        ADUser Install
+        {
+            Ensure     = 'Present'
+            UserName   = Install
+            DomainName = $DomainName
+            Path       = "OU=Office 365,OU=End User,OU=Accounts,$BaseDN"
+            Password = $DomainCreds
+            Enabled = $True
+        }
+
+        ADUser SQLSvc1
+        {
+            Ensure     = 'Present'            
+            UserName   = SQLSvc1
+            DomainName = $DomainName
+            Path       = "OU=Office 365,OU=End User,OU=Accounts,$BaseDN"
+            Password = $DomainCreds
+            Enabled = $True
+        }
+
+        ADUser SQLSvc2
+        {
+            Ensure     = 'Present'            
+            UserName   = SQLSvc2
+            DomainName = $DomainName
+            Path       = "OU=Office 365,OU=End User,OU=Accounts,$BaseDN"
+            Password = $DomainCreds
+            Enabled = $True
+        }
+    }
+}
+
+$configData = @{
+                AllNodes = @(
+                              @{
+                                 NodeName = 'localhost';
+                                 PSDscAllowPlainTextPassword = $true
+                                    }
+                    )
+               }
