2cd080ddde040321fc9b5b4b8999af7563ecf3e5
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-11-26 20:21:17 -0500

    .

--- SQL2019-AlwaysOn/DSC/GRANTLOCALADMIN.ps1
+++ SQL2019-AlwaysOn/DSC/GRANTLOCALADMIN.ps1
@@ -12,7 +12,8 @@
         {
             SetScript =
             {
-                Add-LocalGroupMember -Member "$using:NetBiosDomain\$Using:Account" -Group Administrators
+                $AccountCheck = Get-LocalGroupMember -Group Administrators -Member "$using:Account" -ErrorAction 0
+                IF ($AccountCheck -eq $null){Add-LocalGroupMember -Member "$using:NetBiosDomain\$Using:Account" -Group Administrators}
             }
             GetScript =  { @{} }
             TestScript = { $false}
