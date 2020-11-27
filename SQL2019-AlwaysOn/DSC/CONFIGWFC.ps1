92a70b8a652509e822ca6742e63bc5c58fe3d9d1
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-11-27 14:48:26 -0500

    .

--- SQL2019-AlwaysOn/DSC/CONFIGWFC.ps1
+++ SQL2019-AlwaysOn/DSC/CONFIGWFC.ps1
@@ -6,6 +6,10 @@
         [String]$SQLNode1,     
         [String]$SQLNode2,     
         [String]$NetBiosDomain,
+        [String]$StorageAccountName,
+        [String]$StorageAccountKey,
+        [String]$StorageEndpoint,
+        [String]$BlobService,        
         [System.Management.Automation.PSCredential]$Admincreds
     )
 
@@ -23,6 +27,9 @@
                 # Create Cluster
                 $Cluster = Get-Cluster -Name "$using:SQLClusterName"  -ErrorAction 0
                 IF ($cluster -eq $null) {New-Cluster -Name "$using:SQLClusterName" -Node "$using:SQLNode1","$using:SQLNode2"}
+
+		        # Create Cloud Storage Account
+		        Set-ClusterQuorum -CloudWitness -AccountName "$using:BlobService" -AccessKey "$using:StorageAccountKey" -EndPoint "$using:StorageEndPoint"
             }
             GetScript =  { @{} }
             TestScript = { $false}
