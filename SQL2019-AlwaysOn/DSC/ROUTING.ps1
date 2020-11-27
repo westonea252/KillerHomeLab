6dd7576dc87a5662f7dcd6213f24de0c7edd196f
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-11-27 10:57:58 -0500

    Updated DSC Scripts

--- SQL2019-AlwaysOn/DSC/ROUTING.ps1
+++ SQL2019-AlwaysOn/DSC/ROUTING.ps1
@@ -2,7 +2,8 @@
 {
    param
    (
-        [String]$NextHop
+        [String]$NextHop,
+        [String]$Subnet
     )
 
     Node localhost
@@ -17,9 +18,10 @@
             SetScript =
             {
                 $DG = "'"+$using:NextHop+"'"
+                $IP = "'"+$using:Subnet+"*"+"'"
                 $RouteCheck = Get-NetRoute | Where-Object {$_.NextHop -like $DG}
                 IF ($RouteCheck -eq $Null) {
-                $Adapter = Get-NetIPAddress | Where-Object {$_.IPAddress -like $DG}
+                $Adapter = Get-NetIPAddress | Where-Object {$_.IPAddress -like $IP}
                 New-NetRoute -DestinationPrefix "0.0.0.0/0" -InterfaceIndex $Adapter.InterfaceIndex -NextHop $DG
                 }
             }
