c86671dbd50a7fbf600fe6eeec9c646483ab79f7
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-12-10 14:04:40 -0500

    New Files for New ACL X-Ray Deployment

---
+++ ACL-X-Ray/DSC/RESTARTVM.ps1
@@ -0,0 +1,31 @@
+﻿configuration RESTARTVM
+{
+   param
+   (
+    )
+
+    Node localhost
+    {
+        File Machineconfig
+        {
+            Type = 'Directory'
+            DestinationPath = 'C:\MachineConfig'
+            Ensure = "Present"
+        }
+
+        Script RestartVM
+        {
+            SetScript =
+            {
+                # Restart VM
+                $Path = Get-Item -Path C:\MachineConfig\RebootComplete.txt
+                IF ($Path -eq $Null) {Restart-Computer -Force}
+
+                Set-Content -Path C:\MachineConfig\RebootComplete.txt -Value "Complete"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+        }
+
+    }
+}
