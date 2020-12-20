f453fd28e399ff95ea0d8169000c8ed66e7dc79b
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-12-19 21:41:52 -0500

    Fixed Exchange Install Incremental Logic

--- Exchange2010-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/EXCHANGE10.ps1
+++ Exchange2010-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/EXCHANGE10.ps1
@@ -21,11 +21,8 @@
         {
             SetScript =
             {
-                $Install = Get-ChildItem -Path S:\ExchangeInstall\DeployExchange.cmd -ErrorAction 0
-                IF ($Install -eq $null) {
                 Set-Content -Path S:\ExchangeInstall\DeployExchange.cmd -Value "S:\ExchangeInstall\Exchange2010SP3\Setup.com /roles:mb,ca,ht /mdbname:$using:DBName /dbfilepath:M:\$using:DBName\$using:DBName.edb /logfolderpath:M:\$using:DBName /dc:$using:SetupDC"
                 S:\ExchangeInstall\DeployExchange.cmd
-                }
             }
             GetScript =  { @{} }
             TestScript = { $false}
