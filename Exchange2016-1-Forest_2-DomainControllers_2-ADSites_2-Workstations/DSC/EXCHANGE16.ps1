d6f021299a842509ffba3536ef92206b3901e83a
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-12-28 11:39:36 -0500

    Updated Deployment

--- Exchange2016-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/EXCHANGE16.ps1
+++ Exchange2016-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/EXCHANGE16.ps1
@@ -8,6 +8,8 @@
         [System.Management.Automation.PSCredential]$Admincreds
     )
 
+    Import-DscResource -Module xStorage # Used by Disk
+
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
 
     Node localhost
@@ -17,6 +19,15 @@
             RebootNodeIfNeeded = $true
         }
 
+        Script DismountISO
+        {
+      	    SetScript = {
+                Dismount-DiskImage "S:\ExchangeInstall\Exchange2016.iso" -ErrorAction 0
+            }
+            GetScript =  { @{} }
+            TestScript = { $false }
+        }
+
         File ExchangeInstall
         {
             Type = 'Directory'
@@ -24,16 +35,33 @@
             Ensure = "Present"
         }
 
+        xMountImage MountExchangeISO
+        {
+            ImagePath   = 'S:\ExchangeInstall\Exchange2016.iso'
+            DriveLetter = 'J'
+        }
+
+        xWaitForVolume WaitForISO
+        {
+            DriveLetter      = 'J'
+            RetryIntervalSec = 5
+            RetryCount       = 10
+        }
+
         Script InstallExchange2016
         {
             SetScript =
             {
+                $Install = Get-ChildItem -Path S:\ExchangeInstall\DeployExchange.cmd -ErrorAction 0
+                IF ($Install -eq $null) {   
                 Set-Content -Path S:\ExchangeInstall\DeployExchange.cmd -Value "J:\Setup.exe /Iacceptexchangeserverlicenseterms /Mode:Install /Role:Mailbox /DbFilePath:M:\$using:DBName\$using:DBName.edb /LogFolderPath:M:\$using:DBName /MdbName:$using:DBName /dc:$using:SetupDC"
                 S:\ExchangeInstall\DeployExchange.cmd
+		}
             }
             GetScript =  { @{} }
             TestScript = { $false}
             PsDscRunAsCredential = $DomainCreds
+            DependsOn = '[xWaitForVolume]WaitForISO'
         }
 
     }
