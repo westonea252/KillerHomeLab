cd888b0735043328a05e5170c6dc5c631bd7379b
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-12-30 19:23:14 -0500

    Finalize Master

--- 1-Forest_Parent-Child-Domain_1-Workstation/DSC/CONFIGDNS.ps1
+++ 1-Forest_Parent-Child-Domain_1-Workstation/DSC/CONFIGDNS.ps1
@@ -2,10 +2,13 @@
 {
    param
    (
-        [String]$computerName,
+        [String]$DC1Name,
+        [String]$DC2Name,
         [String]$dc1lastoctet,
+        [String]$dc2lastoctet,
         [String]$domainName,
-        [String]$ReverseLookup1
+        [String]$ReverseLookup1,
+        [String]$ReverseLookup2
     )
 
     Import-DscResource -Module xDnsServer
@@ -20,14 +23,32 @@
             ReplicationScope = 'Forest'
         }
 
+        xDnsServerADZone ReverseADZone2
+        {
+            Name             = "$ReverseLookup2.in-addr.arpa"
+            DynamicUpdate = 'Secure'
+            Ensure           = 'Present'
+            ReplicationScope = 'Forest'
+        }
+
         xDnsRecord DC1PtrRecord
         {
             Name      = "$dc1lastoctet"
             Zone      = "$ReverseLookup1.in-addr.arpa"
-            Target    = "$computerName.$DomainName"
+            Target    = "$DC1Name.$DomainName"
             Type      = 'Ptr'
             Ensure    = 'Present'
             DependsOn = "[xDnsServerADZone]ReverseADZone1"
+        }
+
+        xDnsRecord DC2PtrRecord
+        {
+            Name      = "$dc2lastoctet"
+            Zone      = "$ReverseLookup2.in-addr.arpa"
+            Target    = "$DC2Name.$DomainName"
+            Type      = 'Ptr'
+            Ensure    = 'Present'
+            DependsOn = "[xDnsServerADZone]ReverseADZone2"
         }
     }
 }
