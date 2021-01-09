ec76d7ca00b6bb90be38188c4e6d6af51eba321b
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2021-01-09 18:38:45 -0500

    Finalize Master

---
+++ Azure-Site-Recovery_Same-Address-Space/Scripts/backupvm.ps1
@@ -0,0 +1,29 @@
+﻿Param(
+    [string]$BackupPolicyName,
+    [string]$RSVaultName,
+    [string]$ResourceGroupName,
+    [string]$vmName
+)
+
+# Get Recovery Services Vault Information
+$targetVault = Get-AzRecoveryServicesVault -ResourceGroupName $ResourceGroupName -Name $RSVaultName
+
+# Set Recovery Services Vault to use Geo Redundancy
+Set-AzRecoveryServicesBackupProperty -Vault $targetVault.ID -BackupStorageRedundancy GeoRedundant
+
+# Get Backup Policy
+$schPol = Get-AzRecoveryServicesBackupSchedulePolicyObject -WorkloadType "AzureVM"
+$UtcTime = Get-Date -Date "2019-04-06 01:00:00Z"
+$UtcTime = $UtcTime.ToUniversalTime()
+$schpol.ScheduleRunTimes[0] = $UtcTime
+
+# Get Retention Policy
+$retPol = Get-AzRecoveryServicesBackupRetentionPolicyObject -WorkloadType "AzureVM"
+
+# Create New Policy
+New-AzRecoveryServicesBackupProtectionPolicy -Name $BackupPolicyName -WorkloadType "AzureVM" -RetentionPolicy $retPol -SchedulePolicy $schPol -VaultId $targetVault.ID
+
+$pol = Get-AzRecoveryServicesBackupProtectionPolicy -Name $BackupPolicyName -VaultId $targetVault.ID
+
+# Enable Protection
+Enable-AzRecoveryServicesBackupProtection -Policy $pol -Name $vmName -ResourceGroupName $ResourceGroupName -VaultId $targetVault.ID
