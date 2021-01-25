81c4c02dd913e7e20c6f619644df6fe8d98981cf
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2021-01-25 10:24:33 -0500

    Finalize Development

---
+++ _Troubleshooting/Remove_DSC.ps1
@@ -0,0 +1,5 @@
+﻿$vm = Read-Host "Enter VM Name"
+$Session = New-CimSession -ComputerName $vm
+Remove-DscConfigurationDocument -Stage Previous -CimSession $Session
+Remove-DscConfigurationDocument -Stage Current -CimSession $Session
+Remove-DscConfigurationDocument -Stage Pending -CimSession $Session
