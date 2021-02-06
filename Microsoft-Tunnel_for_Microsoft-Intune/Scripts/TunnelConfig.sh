c64af8d18fb1a7713ab8de48fe4f0f3f92560295
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-02-06 17:09:30 -0500

    Finalize Development

--- Microsoft-Tunnel_for_Microsoft-Intune/Scripts/TunnelConfig.sh
+++ Microsoft-Tunnel_for_Microsoft-Intune/Scripts/TunnelConfig.sh
@@ -1,1 +1,13 @@
-sudo su apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo add-apt-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install --yes docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+sudo wget --output-document=mst-readiness https://aka.ms/microsofttunnelready
+sudo chmod +x ./mst-readiness
+sudo ./mst-readiness network
+sudo apt-get install jq -y
+
+sudo ./mst-readiness account
