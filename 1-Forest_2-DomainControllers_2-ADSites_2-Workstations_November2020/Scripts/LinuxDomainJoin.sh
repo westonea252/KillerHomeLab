c3b79dff18f04a29488fca5eb07d29cf9f6a4eba
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-02-18 19:00:45 -0500

    Finalize Development

--- 1-Forest_2-DomainControllers_2-ADSites_2-Workstations_November2020/Scripts/LinuxDomainJoin.sh
+++ 1-Forest_2-DomainControllers_2-ADSites_2-Workstations_November2020/Scripts/LinuxDomainJoin.sh
@@ -1,17 +1,13 @@
 sudo apt -y update
-
-sudo tee -a /etc/apt/sources.list <<EOF
-deb http://us.archive.ubuntu.com/ubuntu/ bionic universe
-deb http://us.archive.ubuntu.com/ubuntu/ bionic-updates universe
-EOF
-
-sudo apt update
 sudo apt -y install realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
+sudo apt -y upgrade
 
 sudo hostnamectl set-hostname khl-kf-01.dir.ad.killerhomelab.com
 
 sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
+
+sudo unlink /etc/resolv.conf
 
 sudo tee -a /etc/resolv.conf <<EOF
 domain dir.ad.killerhomelab.com
@@ -33,3 +29,4 @@ sudo pam-auth-update
 sudo systemctl restart sssd
 
 sudo realm permit dom-admin@dir.ad.killerhomelab.com
+
