82f47277e7509d885ab5d383294c704f8a44a821
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-03-05 20:18:00 -0500

    Finalize Master

---
+++ Recover-Exchange2016-Server/DSC/PREPARERECOVERY16.ps1
@@ -0,0 +1,29 @@
+﻿configuration PREPARERECOVERY16
+{
+   param
+   (
+        [String]$ComputerName,
+        [String]$NetBiosDomain,
+        [String]$DAGName,
+        [String]$DBName,
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node localhost
+    {
+        Script PrepareRecovery
+        {
+            SetScript =
+            {
+                Remove-MailboxDatabaseCopy "$using:DBName\$using:ComputerName"
+                Remove-DatabaseAvailabilityGroupServer -Identity "$using:DAGName" -MailboxServer "$using:ComputerName"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            PsDscRunAsCredential = $DomainCreds
+        }
+
+    }
+}
