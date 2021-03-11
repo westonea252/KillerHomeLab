dd3dce4435c2b619c9cbc1a79f0949097737cace
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-03-11 07:29:27 -0500

    Finalize Master

--- Exchange2013-with-External-Access-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/CONFIGEXCHANGE13.ps1
+++ Exchange2013-with-External-Access-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/CONFIGEXCHANGE13.ps1
@@ -3,15 +3,13 @@
    param
    (
         [String]$ComputerName,
-        [String]$RootDomainFQDN,                
+        [String]$InternaldomainName,
+        [String]$ExternaldomainName,                                
         [String]$NetBiosDomain,
         [String]$BaseDN,
         [String]$Site1DC,
         [String]$Site2DC,
         [String]$ConfigDC,
-        [String]$Site1FSW,
-        [String]$DAGName,
-        [String]$DAGIPAddress,
         [String]$CAServerIP,
         [String]$Site,
         [System.Management.Automation.PSCredential]$Admincreds
@@ -42,15 +40,15 @@
                 $Password = $DomainCreds.Password
 
                 # Get Certificate 2013 Certificate
-                $CertCheck = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -like "CN=owa2013.$using:RootDomainFQDN"}
-                IF ($CertCheck -eq $Null) {Get-Certificate -Template WebServer1 -SubjectName "CN=owa2013.$using:RootDomainFQDN" -DNSName "owa2013.$using:RootDomainFQDN","autodiscover.$using:RootDomainFQDN","autodiscover2013.$using:RootDomainFQDN","outlook2013.$using:RootDomainFQDN","eas2013.$using:RootDomainFQDN" -CertStoreLocation "cert:\LocalMachine\My"}
+                $CertCheck = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -like "CN=owa2013.$using:ExternaldomainName"}
+                IF ($CertCheck -eq $Null) {Get-Certificate -Template WebServer1 -SubjectName "CN=owa2013.$using:ExternaldomainName" -DNSName "owa2013.$using:ExternaldomainName","autodiscover.$using:ExternaldomainName","autodiscover2013.$using:ExternaldomainName","outlook2013.$using:ExternaldomainName","eas2013.$using:ExternaldomainName" -CertStoreLocation "cert:\LocalMachine\My"}
 
-                $thumbprint = (Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -like "CN=owa2013.$using:RootDomainFQDN"}).Thumbprint
+                $thumbprint = (Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -like "CN=owa2013.$using:ExternaldomainName"}).Thumbprint
                 (Get-ChildItem -Path Cert:\LocalMachine\My\$thumbprint).FriendlyName = "Exchange 2013 SAN Cert"
 
                 # Export Service Communication Certificate
-                $CertFile = Get-ChildItem -Path "C:\Certificates\owa2013.$using:RootDomainFQDN.pfx" -ErrorAction 0
-                IF ($CertFile -eq $Null) {Get-ChildItem -Path cert:\LocalMachine\my\$thumbprint | Export-PfxCertificate -FilePath "C:\Certificates\owa2013.$using:RootDomainFQDN.pfx" -Password $Password}
+                $CertFile = Get-ChildItem -Path "C:\Certificates\owa2013.$using:ExternaldomainName.pfx" -ErrorAction 0
+                IF ($CertFile -eq $Null) {Get-ChildItem -Path cert:\LocalMachine\my\$thumbprint | Export-PfxCertificate -FilePath "C:\Certificates\owa2013.$using:ExternaldomainName.pfx" -Password $Password}
 
                 # Share Certificate
                 $CertShare = Get-SmbShare -Name Certificates -ErrorAction 0
@@ -69,43 +67,37 @@
                 repadmin /replicate "$using:Site2DC" "$using:Site1DC" "$using:BaseDN"
 
                 # Get Certificate 2013 Certificate
-                $thumbprint = (Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -like "CN=owa2013.$using:RootDomainFQDN"}).Thumbprint
+                $thumbprint = (Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -like "CN=owa2013.$using:ExternaldomainName"}).Thumbprint
                 
                 # Connect to Exchange
-                $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$using:computerName.$using:RootDomainFQDN/PowerShell/"
+                $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$using:computerName.$using:InternalDomainName/PowerShell/"
                 Import-PSSession $Session
 
                 # Set Virtual Directories
                 # Set AUTODISCOVER Virtual Directory
-                Set-ClientAccessServer "$using:computerName" –AutodiscoverServiceInternalUri "https://autodiscover2013.$using:RootDomainFQDN/Autodiscover/Autodiscover.xml"
+                Set-ClientAccessServer "$using:computerName" –AutodiscoverServiceInternalUri "https://autodiscover2013.$using:ExternaldomainName/Autodiscover/Autodiscover.xml"
                 
                 # Set OWA Virtual Directory"
-                Set-OWAVirtualDirectory –Identity "$using:computerName\owa (Default Web Site)" –InternalURL "https://owa2013.$using:RootDomainFQDN/OWA" -ExternalURL "https://owa2013.$using:RootDomainFQDN/OWA" -ExternalAuthenticationMethods NTLM -FormsAuthentication:$False -BasicAuthentication:$False –WindowsAuthentication:$True
+                Set-OWAVirtualDirectory –Identity "$using:computerName\owa (Default Web Site)" –InternalURL "https://owa2013.$using:ExternaldomainName/OWA" -ExternalURL "https://owa2013.$using:ExternaldomainName/OWA" -ExternalAuthenticationMethods NTLM -FormsAuthentication:$False -BasicAuthentication:$False –WindowsAuthentication:$True
 
                 # Set ECP Virtual Directory
-                Set-ECPVirtualDirectory –Identity "$using:computerName\ecp (Default Web Site)" –InternalURL "https://owa2013.$using:RootDomainFQDN/ECP" -ExternalURL "https://owa2013.$using:RootDomainFQDN/ECP" -ExternalAuthenticationMethods NTLM -FormsAuthentication:$False -BasicAuthentication:$False –WindowsAuthentication:$True
+                Set-ECPVirtualDirectory –Identity "$using:computerName\ecp (Default Web Site)" –InternalURL "https://owa2013.$using:ExternaldomainName/ECP" -ExternalURL "https://owa2013.$using:ExternaldomainName/ECP" -ExternalAuthenticationMethods NTLM -FormsAuthentication:$False -BasicAuthentication:$False –WindowsAuthentication:$True
 
                 # Set OAB Virtual Directory
-                Set-OABVirtualDirectory –Identity "$using:computerName\oab (Default Web Site)" –InternalURL "https://outlook2013.$using:RootDomainFQDN/OAB" -ExternalURL "https://outlook2013.$using:RootDomainFQDN/OAB"
+                Set-OABVirtualDirectory –Identity "$using:computerName\oab (Default Web Site)" –InternalURL "https://outlook2013.$using:ExternaldomainName/OAB" -ExternalURL "https://outlook2013.$using:ExternaldomainName/OAB"
                 
                 # Set MRS PROXY Virtual Directory"
                 Set-WebServicesVirtualDirectory –Identity "$using:computerName\EWS (Default Web Site)" –MRSProxyEnabled:$True
-                Set-WebServicesVirtualDirectory –Identity "$using:computerName\EWS (Default Web Site)" –InternalURL "https://outlook2013.$using:RootDomainFQDN/EWS/Exchange.asmx" -ExternalURL "https://outlook2013.$using:RootDomainFQDN/EWS/Exchange.asmx"
+                Set-WebServicesVirtualDirectory –Identity "$using:computerName\EWS (Default Web Site)" –InternalURL "https://outlook2013.$using:ExternaldomainName/EWS/Exchange.asmx" -ExternalURL "https://outlook2013.$using:ExternaldomainName/EWS/Exchange.asmx"
 
                 # Set ACTIVESYNC Virtual Directory
-                Set-ActiveSyncVirtualDirectory –Identity "$using:computerName\Microsoft-Server-ActiveSync (Default Web Site)" –InternalURL "https://eas2013.$using:RootDomainFQDN/Microsoft-Server-ActiveSync" -ExternalURL "https://eas2013.$using:RootDomainFQDN/Microsoft-Server-ActiveSync"
+                Set-ActiveSyncVirtualDirectory –Identity "$using:computerName\Microsoft-Server-ActiveSync (Default Web Site)" –InternalURL "https://eas2013.$using:ExternaldomainName/Microsoft-Server-ActiveSync" -ExternalURL "https://eas2013.$using:ExternaldomainName/Microsoft-Server-ActiveSync"
 
                 # Set MAPI Virtual Directory
-                Set-MapiVirtualDirectory –Identity "$using:computerName\mapi (Default Web Site)" –InternalURL "https://outlook2013.$using:RootDomainFQDN/MAPI" -ExternalURL "https://outlook2013.$using:RootDomainFQDN/MAPI" -IISAuthenticationMethods Ntlm,OAuth,Negotiate
+                Set-MapiVirtualDirectory –Identity "$using:computerName\mapi (Default Web Site)" –InternalURL "https://outlook2013.$using:ExternaldomainName/MAPI" -ExternalURL "https://outlook2013.$using:ExternaldomainName/MAPI" -IISAuthenticationMethods Ntlm,OAuth,Negotiate
 
                 # Enable Exchange 2013 Certificate
                 Enable-ExchangeCertificate -Thumbprint $thumbprint -Services IIS -Confirm:$False
-
-                # Create DAG
-                $DAGCheck = Get-DatabaseAvailabilityGroup -Identity "$using:DAGName" -DomainController "$using:ConfigDC" -ErrorAction 0
-                IF ($DAGCheck -eq $null) {New-DatabaseAvailabilityGroup -Name "$using:DAGName" -WitnessServer "$using:Site1FSW" -WitnessDirectory C:\FSWs -DomainController "$using:ConfigDC"}
-                Set-DatabaseAvailabilityGroup -Identity "$using:DAGName" -DatabaseAvailabilityGroupIpAddresses "$using:DAGIPAddress" -DomainController "$using:ConfigDC"
-                Add-DatabaseAvailabilityGroupServer -Identity "$using:DAGName" -MailboxServer "$using:computerName" -DomainController "$using:ConfigDC"
 
                 # Create Connectors
                 $LocalRelayRecieveConnector = Get-ReceiveConnector "LocalRelay $using:computerName" -DomainController "$using:ConfigDC" -ErrorAction 0
@@ -127,6 +119,30 @@
             TestScript = { $false}
             PsDscRunAsCredential = $DomainCreds
             DependsOn = '[Script]ConfigureCertificate'
+        }
+
+        Script ConfigureExchangeSecurityandPerformance
+        {
+            SetScript =
+            {
+                # Set PageFile to Manual
+                $pagefileset = Get-WmiObject Win32_pagefilesetting
+                $pagefileset.InitialSize = 8192
+                $pagefileset.MaximumSize = 32778
+                $pagefileset.Put() | Out-Null
+            
+                # Disable SMB1
+                Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol
+                Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+
+                # Set TCP Keep Alive
+                $tcpkeepalive = get-itemproperty -Path "HKLM:\System\CurrentControlSet\Services\Tcpip\Parameters" -Name "KeepAliveTime" -ErrorAction 0
+                IF ($tcpkeepalive -eq $null) {New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\Tcpip\Parameters\" -Name "KeepAliveTime" -Value 1800000}
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            PsDscRunAsCredential = $DomainCreds
+            DependsOn = '[Script]ConfigureExchange2013'
         }
     }
 }
--- Exchange2013-with-External-Access-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/OTHERDC.ps1
+++ Exchange2013-with-External-Access-1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/OTHERDC.ps1
@@ -2,6 +2,7 @@
 {
    param
     (
+        [String]$TimeZone,        
         [String]$DomainName,
         [String]$DNSServerIP,
         [String]$NetBiosDomain,
@@ -10,12 +11,13 @@
         [Int]$RetryIntervalSec=30
     )
 
-    Import-DscResource -Module xStorage
-    Import-DscResource -Module xNetworking
-    Import-DscResource -Module xPSDesiredStateConfiguration
-    Import-DscResource -Module ComputerManagementDsc
-    Import-DscResource -Module xActiveDirectory
-    Import-DscResource -Module xPendingReboot    
+    Import-DscResource -ModuleName ActiveDirectoryDsc
+    Import-DscResource -ModuleName xStorage
+    Import-DscResource -ModuleName xNetworking
+    Import-DscResource -ModuleName xPendingReboot
+    Import-DscResource -ModuleName ComputerManagementDsc
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+    Import-DscResource -ModuleName xDNSServer    
 
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
 
@@ -71,29 +73,29 @@
             DependsOn="[WindowsFeature]ADDSInstall"
         }
 
-        xWaitForADDomain DscForestWait
+        WaitForADDomain DscForestWait
         {
             DomainName = $DomainName
-            DomainUserCredential= $DomainCreds
-            RetryCount = $RetryCount
-            RetryIntervalSec = $RetryIntervalSec
+            Credential= $DomainCreds
+            RestartCount = $RetryCount
+            WaitTimeout = $RetryIntervalSec
             DependsOn = '[xDNSServerAddress]DnsServerAddress'
         }
-        xADDomainController BDC
+        ADDomainController BDC
         {
             DomainName = $DomainName
-            DomainAdministratorCredential = $DomainCreds
+            Credential = $DomainCreds
             SafemodeAdministratorPassword = $DomainCreds
             DatabasePath = "N:\NTDS"
             LogPath = "N:\NTDS"
             SysvolPath = "N:\SYSVOL"
-            DependsOn = "[xWaitForADDomain]DscForestWait"
+            DependsOn = "[WaitForADDomain]DscForestWait"
         }
 
         TimeZone SetTimeZone
         {
             IsSingleInstance = 'Yes'
-            TimeZone         = 'Eastern Standard Time'
+            TimeZone         = $TimeZone
         }
 
         Script UpdateDNSSettings
