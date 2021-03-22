d39348b8e21c5420daa467188c3030365d913a69
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-03-21 21:22:27 -0400

    Finalize Master

--- 1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/CONFIGDNS.ps1
+++ 1-Forest_2-DomainControllers_2-ADSites_2-Workstations/DSC/CONFIGDNS.ps1
@@ -3,23 +3,38 @@
    param
    (
         [String]$computerName,
+        [String]$NetBiosDomain,
+        [String]$InternaldomainName,
         [String]$dc1lastoctet,
         [String]$dc2lastoctet,
         [String]$domainName,
         [String]$ReverseLookup1,
-        [String]$ReverseLookup2
+        [String]$ReverseLookup2,
+        [Int]$RetryIntervalSec=420,
+        [System.Management.Automation.PSCredential]$Admincreds
     )
 
     Import-DscResource -Module xDnsServer
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
 
     Node localhost
     {
+        WaitForADDomain DscForestWait
+        {
+            DomainName = $InternaldomainName
+            Credential= $DomainCreds
+            WaitTimeout = $RetryIntervalSec
+        }
+
         xDnsServerADZone ReverseADZone1
         {
             Name             = "$ReverseLookup1.in-addr.arpa"
             DynamicUpdate = 'Secure'
             Ensure           = 'Present'
             ReplicationScope = 'Domain'
+            DependsOn = '[WaitForADDomain]DscForestWait'
         }
 
         xDnsServerADZone ReverseADZone2
@@ -28,6 +43,7 @@
             DynamicUpdate = 'Secure'
             Ensure           = 'Present'
             ReplicationScope = 'Domain'
+            DependsOn = '[WaitForADDomain]DscForestWait'
         }
 
         xDnsRecord DC1PtrRecord
