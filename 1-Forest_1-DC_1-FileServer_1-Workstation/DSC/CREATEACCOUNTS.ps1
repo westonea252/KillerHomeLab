4ed02eb4f66a13a490a493d92aada68bca7d7340
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-04-02 18:32:00 -0400

    Finalize Development

--- 1-Forest_1-DC_1-FileServer_1-Workstation/DSC/CREATEACCOUNTS.ps1
+++ 1-Forest_1-DC_1-FileServer_1-Workstation/DSC/CREATEACCOUNTS.ps1
@@ -3,7 +3,6 @@
    param
    (
         [String]$UserCount,
-        [String]$UserDataUrl,
         [String]$NetBiosDomain,
         [String]$DomainName,        
         [String]$OUPath,      
@@ -11,47 +10,12 @@
     )
 
     Import-DscResource -Module ActiveDirectoryDsc
-    Import-DscResource -Module xPSDesiredStateConfiguration # Used for xRemoteFile
 
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
 
     Node $AllNodes.NodeName
 
 {
-        Registry SchUseStrongCrypto
-        {
-            Key                         = 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319'
-            ValueName                   = 'SchUseStrongCrypto'
-            ValueType                   = 'Dword'
-            ValueData                   =  '1'
-            Ensure                      = 'Present'
-        }
-
-        Registry SchUseStrongCrypto64
-        {
-            Key                         = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319'
-            ValueName                   = 'SchUseStrongCrypto'
-            ValueType                   = 'Dword'
-            ValueData                   =  '1'
-            Ensure                      = 'Present'
-        }
-
-        File CreateDataFolder
-        {
-            Type = 'Directory'
-            DestinationPath = "C:\Data"
-            Ensure = "Present"
-            DependsOn = '[Registry]SchUseStrongCrypto','[Registry]SchUseStrongCrypto64'
-        }
-
-        xRemoteFile DownloadUserData
-        {
-            DestinationPath = "C:\Data\UserData.zip"
-            Uri             = $UserDataUrl
-            UserAgent       = "[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer"
-            DependsOn = '[File]CreateDataFolder'
-        }
-
         $Number = 0
         foreach($Item in 1..$UserCount)
         {
@@ -66,31 +30,6 @@
                 Path       = "$OUPath"
                 Password = $DomainCreds
                 Enabled = $True
-            }
-
-            File "CreateUserFolder$Number"
-            {
-                Type = 'Directory'
-                DestinationPath = "C:\Users\$UserName"
-                Ensure = "Present"
-                DependsOn = "[ADUser]CreateUser$Number","[Registry]SchUseStrongCrypto64"
-            }
-
-            Script UnzipUserData
-            {
-                SetScript =
-                {
-                    # UnCompress UserFolder
-                    $UserFolderPath = Get-Item -Path "C:\Users\$using:UserName" -ErrorAction 0
-                    IF ($UserFolderPath -ne $Null) {
-                    Expand-Archive -Path "C:\Data\UserData.zip" -DestinationPath "C:\Users\$using:UserName"
-                    }
-                    ELSE {}
-                }
-                GetScript =  { @{} }
-                TestScript = { $false}
-                PsDscRunAsCredential = $DomainCreds
-                DependsOn = "[ADUser]CreateUserFolder$Number"
             }
         }
     }
---
+++ 1-Forest_1-DC_1-FileServer_1-Workstation/DSC/CREATEUSERFOLDERS.ps1
@@ -0,0 +1,83 @@
+﻿Configuration CREATESUSERFOLDERS
+{
+   param
+   (
+        [String]$UserCount,
+        [String]$UserDataUrl,
+        [String]$NetBiosDomain,   
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+
+    Import-DscResource -Module xPSDesiredStateConfiguration # Used for xRemoteFile
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node $AllNodes.NodeName
+
+{
+        File CreateDataFolder
+        {
+            Type = 'Directory'
+            DestinationPath = "C:\Data"
+            Ensure = "Present"
+        }
+
+        Registry SchUseStrongCrypto
+        {
+            Key                         = 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319'
+            ValueName                   = 'SchUseStrongCrypto'
+            ValueType                   = 'Dword'
+            ValueData                   =  '1'
+            Ensure                      = 'Present'
+        }
+
+        Registry SchUseStrongCrypto64
+        {
+            Key                         = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319'
+            ValueName                   = 'SchUseStrongCrypto'
+            ValueType                   = 'Dword'
+            ValueData                   =  '1'
+            Ensure                      = 'Present'
+        }
+
+        xRemoteFile DownloadUserData
+        {
+            DestinationPath = "C:\Data\UserData.zip"
+            Uri             = $UserDataUrl
+            UserAgent       = "[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer"
+            DependsOn = '[File]CreateDataFolder','[Registry]SchUseStrongCrypto','[Registry]SchUseStrongCrypto64'
+        }
+
+        $Number = 0
+        foreach($Item in 1..$UserCount)
+        {
+            $Number += 1
+            $UserName = "User$Number"
+
+            File "CreateUserFolder$Number"
+            {
+                Type = 'Directory'
+                DestinationPath = "C:\Users\$UserName"
+                Ensure = "Present"
+                DependsOn = '[File]CreateDataFolder'
+            }
+
+            Script UnzipUserData
+            {
+                SetScript =
+                {
+                    # UnCompress UserFolder
+                    $UserFolderPath = Get-Item -Path "C:\Users\$using:UserName" -ErrorAction 0
+                    IF ($UserFolderPath -ne $Null) {
+                    Expand-Archive -Path "C:\Data\UserData.zip" -DestinationPath "C:\Users\$using:UserName"
+                    }
+                    ELSE {}
+                }
+                GetScript =  { @{} }
+                TestScript = { $false}
+                PsDscRunAsCredential = $DomainCreds
+                DependsOn = "[ADUser]CreateUserFolder$Number"
+            }
+        }
+    }
+}
