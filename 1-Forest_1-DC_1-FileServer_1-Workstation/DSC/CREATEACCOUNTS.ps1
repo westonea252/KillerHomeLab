b042a846bf8c292a47fce3a6f8c07240ed06a7ab
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-04-02 12:23:23 -0400

    Finalize Development

---
+++ 1-Forest_1-DC_1-FileServer_1-Workstation/DSC/CREATEACCOUNTS.ps1
@@ -0,0 +1,45 @@
+﻿Configuration CREATEACCOUNTS
+{
+   param
+   (
+        [String]$UserCount,
+        [String]$NetBiosDomain,
+        [String]$DomainName,        
+        [String]$OUPath,      
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node $AllNodes.NodeName
+
+{
+    $Number = 0
+    foreach($Item in 1..$UserCount)
+    {
+        $Number += 1
+        $UserName = "User$Number"
+
+        ADUser CreateUsers
+        {
+            Ensure     = 'Present'            
+            UserName   = "$UserName"
+            DomainName = $DomainName
+            Path       = "$OUPath"
+            Password = $DomainCreds
+            Enabled = $True
+        }
+    }
+}
+}
+
+$configData = @{
+                AllNodes = @(
+                              @{
+                                 NodeName = 'localhost';
+                                 PSDscAllowPlainTextPassword = $true
+                                    }
+                    )
+               }
