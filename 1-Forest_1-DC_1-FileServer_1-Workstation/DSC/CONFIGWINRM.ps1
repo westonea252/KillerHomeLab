33dbd2a13d09b4468c0b0147b7414736cb990999
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-04-03 07:22:03 -0400

    Finalize Development

---
+++ 1-Forest_1-DC_1-FileServer_1-Workstation/DSC/CONFIGWINRM.ps1
@@ -0,0 +1,22 @@
+﻿Configuration CONFIGWINRM
+{
+   param
+   (
+        [String]$MaxEnvelopeSizeinKB
+    )
+
+    Node LocalHost
+    {
+
+        Script UpdateWinRMSettings
+        {
+            SetScript =
+            {
+                # Set WinRM MaxEnvelopeSize
+                Set-Item -Path WSMan:\localhost\MaxEnvelopeSizekb -Value $MaxEnvelopeSizeinKB
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+        }
+    }
+}
