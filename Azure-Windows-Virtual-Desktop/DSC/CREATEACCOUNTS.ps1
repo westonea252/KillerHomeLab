ddfc62855a56d5f30fa84a95441a4601e2e521fc
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-04-09 17:27:20 -0400

    Finalize Development

---
+++ Azure-Windows-Virtual-Desktop/DSC/CREATEACCOUNTS.ps1
@@ -0,0 +1,49 @@
+﻿Configuration CREATEACCOUNTS
+{
+   param
+   (
+        [String]$UserCount,
+        [String]$NamingConvention,        
+        [String]$NetBiosDomain,
+        [String]$DomainName,        
+        [String]$OUPath,
+        [String]$fs1name,              
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node $AllNodes.NodeName
+
+{
+        $Number = 0
+        foreach($Item in 1..$UserCount)
+        {
+            $Number += 1
+            $UserName = "$NamingConvention-User$Number"
+
+            ADUser "CreateUser$Number"
+            {
+                Ensure     = 'Present'            
+                UserName   = "$UserName"
+                DomainName = $DomainName
+                Path       = "$OUPath"
+                Password = $DomainCreds
+                Enabled = $True
+                HomeDirectory = "\\$fs1name\HomeDrives\$Username"
+                HomeDrive = "H:"
+            }
+        }
+    }
+}
+
+$configData = @{
+                AllNodes = @(
+                              @{
+                                 NodeName = 'localhost';
+                                 PSDscAllowPlainTextPassword = $true
+                                    }
+                    )
+               }
---
+++ Azure-Windows-Virtual-Desktop/DSC/GRANTRDPUSERS.ps1
@@ -0,0 +1,32 @@
+﻿configuration GRANTRDPUSERS
+{
+   param
+   (
+        [String]$TimeZone,        
+        [String]$Account,
+        [String]$NetBiosDomain
+    )
+
+    Import-DscResource -ModuleName ComputerManagementDsc
+
+    Node localhost
+    {
+        TimeZone SetTimeZone
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = $TimeZone
+        }
+
+        Script GrantRemoteDesktopUsers
+        {
+            SetScript =
+            {
+                $AccountCheck = Get-LocalGroupMember -Group "Remote Desktop Users" -Member "$using:NetBiosDomain\$Using:Account" -ErrorAction 0
+                IF ($AccountCheck -eq $null){Add-LocalGroupMember -Member "$using:NetBiosDomain\$Using:Account" -Group Administrators}
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+        }
+
+    }
+}
