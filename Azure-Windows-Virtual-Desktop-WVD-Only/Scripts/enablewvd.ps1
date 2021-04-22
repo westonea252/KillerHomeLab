ad92683c77d375023ddec4b9a1bac91d7388c10e
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-04-22 18:42:59 -0400

    Finalize Development

--- Azure-Windows-Virtual-Desktop-WVD-Only/Scripts/enablewvd.ps1
+++ Azure-Windows-Virtual-Desktop-WVD-Only/Scripts/enablewvd.ps1
@@ -1,16 +1,10 @@
 ﻿param(
     [String] [Parameter(Mandatory=$true)] $NamingConvention,
     [String] [Parameter(Mandatory=$true)] $TenantName,
-    [String] [Parameter(Mandatory=$true)] $SubscriptionID,    
-    [String] [Parameter(Mandatory=$true)] $AzureAdmin,
-    [SecureString] [Parameter(Mandatory=$true)] $AzurePassword    
+    [String] [Parameter(Mandatory=$true)] $SubscriptionID
     )
 
-[System.Management.Automation.PSCredential ]$Creds = New-Object System.Management.Automation.PSCredential ("$($AzureAdmin)", $AzurePassword)
-
 $Tenant = Get-AzTenant | Where-Object {$_.Name -like "$TenantName"}
-
-Connect-AzAccount -Credential $Creds
 
 Install-Module Microsoft.Rdinfra.RdPowerShell -Force
 
