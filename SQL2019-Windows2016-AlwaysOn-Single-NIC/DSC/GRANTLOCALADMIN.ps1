ee35091fe6e02cd07f0560625b5384af1ba5f134
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-04-29 11:46:06 -0400

    Finalize Development

---
+++ SQL2019-Windows2016-AlwaysOn-Single-NIC/DSC/GRANTLOCALADMIN.ps1
@@ -0,0 +1,32 @@
+﻿configuration GRANTLOCALADMIN
+{
+   param
+   (
+        [String]$TimeZone,        
+        [String]$Account,
+        [String]$NetBiosDomain
+    )
+
+    Import-DscResource -ModuleName ComputerManagementDsc
+
+    Node localhost
+    {
+        TimeZone SetTimeZone
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = $TimeZone
+        }
+
+        Script GrantLocalAdmin
+        {
+            SetScript =
+            {
+                $AccountCheck = Get-LocalGroupMember -Group Administrators -Member "$using:NetBiosDomain\$Using:Account" -ErrorAction 0
+                IF ($AccountCheck -eq $null){Add-LocalGroupMember -Member "$using:NetBiosDomain\$Using:Account" -Group Administrators}
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+        }
+
+    }
+}
