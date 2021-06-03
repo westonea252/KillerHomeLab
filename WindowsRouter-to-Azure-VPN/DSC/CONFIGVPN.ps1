7570ef1d295989b6e22df16fdab364644f8b0af3
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-06-02 21:03:33 -0400

    Finalize Development

--- WindowsRouter-to-Azure-VPN/DSC/CONFIGVPN.ps1
+++ WindowsRouter-to-Azure-VPN/DSC/CONFIGVPN.ps1
@@ -25,13 +25,13 @@
                 # Configure RRAS
                 $Feature = Get-WindowsFeature -Name Routing
                 $Installed = $Feature.Installed
-                $AddressPrefix = "$using:LocalAddressPrefix"
+                $AddressPrefix = "$using:LocalAddressPrefix"+':100'
                 $GatewayIP = "$using:RemoteGatewayIP"
 
                 IF ($Installed -like "False" -or $Null){
                 Install-WindowsFeature Routing -IncludeAllSubFeature -IncludeManagementTools
                 Install-RemoteAccess -VpnType VpnS2S
-                Add-VpnS2SInterface -Protocol IKEv2 -AuthenticationMethod PSKOnly -NumberOfTries 3 -ResponderAuthenticationMethod PSKOnly -Name Azure -Destination "$GatewayIP" -IPv4Subnet @("$AddressPrefix:100") -SharedSecret "$using:SharedKey"
+                Add-VpnS2SInterface -Protocol IKEv2 -AuthenticationMethod PSKOnly -NumberOfTries 3 -ResponderAuthenticationMethod PSKOnly -Name Azure -Destination "$GatewayIP" -IPv4Subnet @("$AddressPrefix") -SharedSecret "$using:SharedKey"
                 Set-VpnServerIPsecConfiguration -EncryptionType MaximumEncryption
                 Set-VpnS2Sinterface -Name Azure -InitiateConfigPayload $false -Force
                 Connect-VpnS2SInterface -Name Azure
