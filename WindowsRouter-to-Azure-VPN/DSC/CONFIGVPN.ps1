89151f5c82c02cd4fde75369ce17e4ccac6e85f1
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-06-24 12:14:33 -0400

    Finalize Development

--- WindowsRouter-to-Azure-VPN/DSC/CONFIGVPN.ps1
+++ WindowsRouter-to-Azure-VPN/DSC/CONFIGVPN.ps1
@@ -30,11 +30,19 @@
 
                 IF ($Installed -like "False" -or $Null){
                 Install-WindowsFeature Routing -IncludeAllSubFeature -IncludeManagementTools
+                Set-Content -Path C:\VPNInstall.txt -Value 'Installed RRAS Role'
                 Install-RemoteAccess -VpnType VpnS2S
+                Add-Content -Path C:\VPNInstall.txt -Value 'Installed VpnS2S'
+                Import-Module RemoteAccess
+                Add-Content -Path C:\VPNInstall.txt -Value 'Imported RemoteAccess Module'
                 Add-VpnS2SInterface -Protocol IKEv2 -AuthenticationMethod PSKOnly -NumberOfTries 3 -ResponderAuthenticationMethod PSKOnly -Name Azure -Destination $GatewayIP -IPv4Subnet $AddressPrefix -SharedSecret "$using:SharedKey"
+                Add-Content -Path C:\VPNInstall.txt -Value 'Added VPNS2SInterface'
                 Set-VpnServerIPsecConfiguration -EncryptionType MaximumEncryption
+                Add-Content -Path C:\VPNInstall.txt -Value 'Set Encryption'
                 Set-VpnS2Sinterface -Name Azure -InitiateConfigPayload $false -Force
+                Add-Content -Path C:\VPNInstall.txt -Value 'Set VPNS2SInterface'
                 Connect-VpnS2SInterface -Name Azure
+                Add-Content -Path C:\VPNInstall.txt -Value 'Connected to Azure'
                 }
             }
             GetScript =  { @{} }
