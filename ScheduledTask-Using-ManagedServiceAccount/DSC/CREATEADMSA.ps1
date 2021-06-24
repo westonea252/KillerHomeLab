b501b5bd3ccc03f44f79be080375c98b70b6c456
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-06-24 06:41:20 -0400

    Finalize Development

---
+++ ScheduledTask-Using-ManagedServiceAccount/DSC/CREATEADMSA.ps1
@@ -0,0 +1,31 @@
+﻿configuration CREATEADMSA
+{
+   param
+   (
+        [String]$NetBiosDomain,
+        [String]$domainName,
+        [String]$computerName,
+        [String]$ServiceAccount
+    )
+
+    Node localhost
+    {
+        Script IssueCARequest
+        {
+            SetScript =
+            {
+                # Create KDS Root Key
+                $rootkey = Get-KdsRootKey
+                IF ($rootkey -eq $nullvar){Add-KdsRootKey -EffectiveTime (Get-Date).AddHours(-10)}
+     
+                # Create AD Managed Service Account
+                $gmsa = Get-ADServiceAccount -Filter * | Where-Object {$_.Name -like "$using:ServiceACcount"}
+                IF ($gmsa -eq $nullvar){New-ADServiceAccount "$using:ServiceACcount" -DNSHostName "$using:computerName.$using:domainName"}
+
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+        }
+
+    }
+}
