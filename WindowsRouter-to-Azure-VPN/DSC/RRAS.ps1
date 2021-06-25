f3d034133060235d8c728941cdd1704470559d99
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-06-24 20:21:46 -0400

    Finalize Development

---
+++ WindowsRouter-to-Azure-VPN/DSC/RRAS.ps1
@@ -0,0 +1,18 @@
+﻿Configuration RRAS
+{
+    Node localhost
+    {   
+        WindowsFeature Routing
+        { 
+            Ensure = 'Present' 
+            Name = 'Routing' 
+        }         
+
+        WindowsFeature RSAT-RemoteAccess
+        { 
+            Ensure = 'Present' 
+            Name = 'RSAT-RemoteAccess' 
+            DependsOn = '[WindowsFeature]Routing'
+        }
+     }
+  }
