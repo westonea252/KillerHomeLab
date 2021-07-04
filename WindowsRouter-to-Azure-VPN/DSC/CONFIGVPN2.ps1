25ae268b4ceda5a15303fe428dbb35aeb8f62551
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-07-03 21:09:26 -0400

    Finalize Development

--- WindowsRouter-to-Azure-VPN/DSC/CONFIGVPN2.ps1
+++ WindowsRouter-to-Azure-VPN/DSC/CONFIGVPN2.ps1
@@ -36,6 +36,7 @@
                 # Create ConfigureRRAS Script
                 $FalseValue = '$False'
                 Set-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Install-RemoteAccess -VpnType VpnS2S"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Import-Module RemoteAccess"
                 Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Add-VpnS2SInterface -Protocol IKEv2 -AuthenticationMethod PSKOnly -NumberOfTries 3 -ResponderAuthenticationMethod PSKOnly -Name Azure -Destination $using:RemoteGatewayIP -IPv4Subnet $using:IPv4Subnet -SharedSecret $using:SharedKey"
                 Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Set-VpnServerIPsecConfiguration -EncryptionType MaximumEncryption"
                 Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Set-VpnS2Sinterface -Name Azure -InitiateConfigPayload $FalseValue -Force"
@@ -58,6 +59,19 @@
             ExecuteAsCredential = $LocalCreds
             LogonType           = 'Password'
             DependsOn = '[Script]ConfigureRRAS'
+        }
+
+        Script AllowLegacy
+        {
+            SetScript =
+            {
+                # Allow Remote Copy
+                $allowlegacy = get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteAccess\Parameters" -Name "ModernStackEnabled" -ErrorAction 0
+                IF ($allowlegacy.ModernStackEnabled -ne 0) {New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteAccess\Parameters\" -Name "ModernStackEnabled" -Value 0 -Force}
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = '[ScheduledTask]CreateConfigureRRAS'
         }
 
     }
