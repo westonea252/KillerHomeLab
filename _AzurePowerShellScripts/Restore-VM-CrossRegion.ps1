f63e72a42e74698c0f4af597a0ca6af73e97a2d7
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-07-09 08:39:13 -0400

    Finalize Development

--- _AzurePowerShellScripts/Restore-VM-CrossRegion.ps1
+++ _AzurePowerShellScripts/Restore-VM-CrossRegion.ps1
@@ -2,93 +2,72 @@
 
 # Select Subscription
 $Subscriptions = Get-AzSubscription | Select-Object Name, ID
-$Subscription = $Subscriptions | Out-GridView -PassThru
+$Subscription = $Subscriptions | Out-GridView -PassThru -Title "Please Select the proper Subscription"
+Select-AzSubscription -Subscription $Subscription.Name
 
-# Write-Host "Example: USGov Virginia"
-# $dstlocation = Read-Host "Please enter Restore Location for the VM Restore"
-$rstlocation = 'USGov Virginia'
+# Select Destination Location
+$dstLocations = Get-AzLocation | Select-Object DisplayName
+$selectdstLocation = $dstLocations | Out-GridView -PassThru -Title "Please Select Destination Location for the VM Restore"
+$dstlocationName = $selectdstLocation.DisplayName
 
-# Write-Host "Example: USGov Texas"
-# $dstlocation = Read-Host "Please enter Destination Location for the VM Restore"
-$dstlocation = 'USGov Texas'
-
-# Write-Host "Example:  ASR-Test-GovTX"
-# $dstVMRG = Read-Host "Please enter VM's Destination Resource Group"
-$dstVMRG = 'ASR-Test-GovTX'
+# Select VM Destination Resource Group
+$dstRGs = Get-AzResourceGroup -Location $dstlocationName | Select-Object ResourceGroupName
+$selectdstRG = $dstRGs | Out-GridView -PassThru -Title "Please Select Destination Resource Group"
+$dstRGName = $selectdstRG.ResourceGroupName
 
 # Select VM
-# Write-Host "Example:  khl-srv-01"
-# $vmName = Read-Host "Please enter VM Name"
-$vmName = 'khl-srv-01'
+Write-Host "Example:  khl-srv-01"
+$vmName = Read-Host "Please enter VM Name"
 
-# Write-Host "Example:  ASR-Test-GovTX-VNet1"
-# $dstvNetName = Read-Host "Please enter VM's Destination Virtual Network Name"
-$dstvNetName = 'ASR-Test-GovTX-VNet1'
+# Select Destination Virtual Network
+$dstvNets = Get-AzVirtualNetwork | Select-Object Name,ResourceGroupName
+$selectdstVNet = $dstvNets | Out-GridView -PassThru -Title "Please Select Destination Virtual Network"
+$dstvNetName = $selectdstVNet.Name
 
-# Write-Host "Example:  ASR-Test-GovTX"
-# $dstvNetNameRG = Read-Host "Please enter VM's Destination Virtual Network Resource Group Name"
-$dstvNetNameRG = 'ASR-Test-GovTX'
+# Select Recovery Services Vault
+$RSVaults = Get-AzRecoveryServicesVault | Select-Object Name
+$selectRSVault = $RSVaults | Out-GridView -PassThru -Title "Please Select the Recovery Service Vault where the Backup was Performed"
+$RSVaultName = $selectRSVault.Name
 
-# Write-Host "Example: khl-rv1-1234567890x36"
-# $RSVault = Read-Host "Please enter Recovery Services Vault Name"
-$RSVault = 'khl-rv1-1234567890x36'
+# Select Restore Storage Account
+$rstStorageAccounts = Get-AzStorageAccount | Select-Object StorageAccountName,ResourceGroupName,Id
+$selectrstStorageAccount = $rstStorageAccounts | Out-GridView -PassThru -Title "Please Select the Storage Account to send Restores"
+$rstStorageAccountName = $selectrstStorageAccount.StorageAccountName
 
-# Write-Host "Example: vabackuptest"
-# $rstStorageAccountName =  Read-Host "Please enter Restore Storage Account Name"
-$rstStorageAccountName = 'vabackuptest'
+# Select Destination Storage Account
+$dstStorageAccounts = Get-AzStorageAccount | Select-Object StorageAccountName,ResourceGroupName,Id
+$selectdstStorageAccount = $dstStorageAccounts | Out-GridView -PassThru -Title "Please Select the Destination Storage Account"
+$dstStorageAccountName = $selectdstStorageAccount.StorageAccountName
 
-# Write-Host "Example: ASR-Test-GovVA"
-# $rstStorageAccountNameRG =  Read-Host "Please enter Restore Storage Account Resource Group Name"
-$rstStorageAccountNameRG = 'ASR-Test-GovVA'
+Write-Host "Example: C:\JSONs"
+$jsonfileroot = Read-Host "Please enter the root location of the JSONFile used for VM Configuration"
 
-# Write-Host "Example: txbackuptest"
-# $dstStorageAccountName =  Read-Host "Please enter Destination Storage Account Name"
-$dstStorageAccountName = 'txbackuptest'
-
-# Write-Host "Example: ASR-Test-GovTX"
-# $dstStorageAccountNameRG =  Read-Host "Please enter Destination Storage Account Resource Group Name"
-$dstStorageAccountNameRG = 'ASR-Test-GovTX'
-
-# Write-Host "Example: vmmigration"
-# $dstCN = Read-Host "Please enter Destination Storage Account Container Name"
-$dstCN = 'vmmigration'
-
-# Write-Host "Example: C:\JSONs"
-# $jsonfileroot = Read-Host "Please enter the root location of the JSONFile used for VM Configuration"
-$jsonfileroot = 'C:\JSONs\'
 $checkjsonfileroot = Get-Item -Path $jsonfileroot -ErrorAction 0
-IF ($checkjsonfileroot -eq $null) {New-Item -ItemType Directory -Path $jsonfileroot -Force}
-$jsonfile = $jsonfileroot+$vmName+'-Config.json'
-
-# Write-Host "Last Recovery Point      = 0)"
-# Write-Host "2 Day Old Recovery Point = 1)"
-# Write-Host "3 Day Old Recovery Point = 2)"
-# $rpdays = Read-Host "Last Recovery Point in Days"
-$rpdays = 0
+IF ($null -eq $checkjsonfileroot) {New-Item -ItemType Directory -Path $jsonfileroot -Force}
+$jsonfile = $jsonfileroot+'\'+$vmName+'-Config.json'
 
 $Date = (Get-Date)
 $DateStr = $Date.ToString("yyyyMMddmmss")
 $storageType = "Standard_LRS"
 
 # Set Source Storage Context
-# Create Restore Storage Account
-New-AzStorageAccount -ResourceGroupName $rstStorageAccountNameRG -Name $rstStorageAccountName -Location $rstlocation -SkuName $storageType -Kind StorageV2
-$rstStorageAccountNameKey = Get-AzStorageAccountKey -ResourceGroupName $rstStorageAccountNameRG -StorageAccountName $rstStorageAccountName
-$rstContext = New-AzStorageContext -StorageAccountName $rstStorageAccountName -StorageAccountKey $rstStorageAccountNameKey.Value[0]
-Set-AzCurrentStorageAccount -StorageAccountName $rstStorageAccountName -ResourceGroupName $rstStorageAccountNameRG
+$rstStorageAccountNameKey = Get-AzStorageAccountKey -ResourceGroupName $selectrstStorageAccount.ResourceGroupName -StorageAccountName $rstStorageAccountName
+$rstContext = New-AzStorageContext -StorageAccountName $rstStorageACcountName -StorageAccountKey $rstStorageAccountNameKey.Value[0]
+Set-AzCurrentStorageAccount -StorageAccountName $rstStorageAccountName -ResourceGroupName $selectrstStorageAccount.ResourceGroupName
 
 # Azure Restore
-Get-AzRecoveryServicesVault -Name $RSVault | Set-AzRecoveryServicesVaultContext
+Get-AzRecoveryServicesVault -Name $RSVaultName | Set-AzRecoveryServicesVaultContext
 $namedContainer = Get-AzRecoveryServicesBackupContainer  -ContainerType "AzureVM" -Status "Registered" | Where-Object {$_.FriendlyName -like $vmName+'*'}
 $backupitem = Get-AzRecoveryServicesBackupItem -Container $namedContainer -WorkloadType "AzureVM"
 $startDate = (Get-Date).AddDays(-7)
 $endDate = Get-Date
-$rp = Get-AzRecoveryServicesBackupRecoveryPoint -Item $backupitem -StartDate $startdate.ToUniversalTime() -EndDate $enddate.ToUniversalTime()
-$restorejob = Restore-AzRecoveryServicesBackupItem -RecoveryPoint $rp[0] -StorageAccountName $rstStorageAccountName -StorageAccountResourceGroupName $rstStorageAccountNameRG  -RestoreAsUnmanagedDisks
+$recoverypoints = Get-AzRecoveryServicesBackupRecoveryPoint -Item $backupitem -StartDate $startdate.ToUniversalTime() -EndDate $enddate.ToUniversalTime()
+$selectrecoverypoint = $RecoveryPoints | Out-GridView -PassThru -Title "Please Select Recovery Point"
+$restorejob = Restore-AzRecoveryServicesBackupItem -RecoveryPoint $selectrecoverypoint -StorageAccountName $rstStorageAccountName -StorageAccountResourceGroupName $selectrstStorageAccount.ResourceGroupName  -RestoreAsUnmanagedDisks
 
 while (($RestoreJob.Status -eq 'InProgress')){
 $RestoreJob = Get-AzRecoveryServicesBackupJob -Job $RestoreJob
-sleep 60}
+Start-Sleep 60}
 
 # Gather Restore Job Details
 $restorejob = Get-AzRecoveryServicesBackupJob -Job $restorejob
@@ -104,37 +83,31 @@ $obj = ((Get-Content -Path $destination_path -Raw -Encoding Unicode)).TrimEnd([c
 $Blob0 = $obj.'properties.StorageProfile'.osDisk.vhd.uri.Split("/")[4]
 
 # Set Destination Variables
-$dstVNet = Get-AzVirtualNetwork -Name $dstvNetName -ResourceGroupName $dstvNetNameRG
-
-# Create Destination Storage Account
-New-AzStorageAccount -ResourceGroupName $dstStorageAccountNameRG -Name $dstStorageAccountName -Location $dstlocation -SkuName $storageType -Kind StorageV2
+$dstVNet = Get-AzVirtualNetwork -Name $dstvNetName -ResourceGroupName $selectdstVNet.ResourceGroupName
 
 # Set Destination Storage Context
-$dstStorageAccountNameKey = Get-AzStorageAccountKey -ResourceGroupName $dstStorageAccountNameRG -StorageAccountName $dstStorageAccountName
+$dstStorageAccountNameKey = Get-AzStorageAccountKey -ResourceGroupName $selectdstStorageAccount.ResourceGroupName -StorageAccountName $dstStorageAccountName
 $dstContext = New-AzStorageContext -StorageAccountName $dstStorageAccountName -StorageAccountKey $dstStorageAccountNameKey.Value[0]
-Set-AzCurrentStorageAccount -StorageAccountName $dstStorageAccountName -ResourceGroupName $dstStorageAccountNameRG
-"$dstCN" | New-AzStorageContainer -Permission Off
+Set-AzCurrentStorageAccount -StorageAccountName $dstStorageAccountName -ResourceGroupName $selectdstStorageAccount.ResourceGroupName
+$srcCN | New-AzStorageContainer -Permission Off
 
 # Copy OS Disk
-Start-AzStorageBlobCopy -SrcContainer $srcCN -DestContainer $dstCN -SrcBlob $Blob0 -DestBlob $Blob0 -Context $rstContext -DestContext $dstContext
+Start-AzStorageBlobCopy -SrcContainer $srcCN -DestContainer $srcCN -SrcBlob $Blob0 -DestBlob $Blob0 -Context $rstContext -DestContext $dstContext
 
 # Copy Data Disks
 foreach($dd in $obj.'properties.StorageProfile'.DataDisks)
   {
-  Start-AzStorageBlobCopy -SrcContainer $srcCN -DestContainer $dstCN -SrcBlob $dd.vhd.Uri.Split("/")[4] -DestBlob $dd.vhd.Uri.Split("/")[4] -Context $rstContext -DestContext $dstContext
+  Start-AzStorageBlobCopy -SrcContainer $srcCN -DestContainer $srcCN -SrcBlob $dd.vhd.Uri.Split("/")[4] -DestBlob $dd.vhd.Uri.Split("/")[4] -Context $rstContext -DestContext $dstContext
   }
 
 # Wait for OS Disk to Finish Copying
-Get-AzStorageBlobCopyState -Blob $Blob0 -Container $dstCN -WaitForComplete
+Get-AzStorageBlobCopyState -Blob $Blob0 -Container $srcCN -WaitForComplete
 
 # Wait for Data Disks to Finish Copying
 foreach($dd in $obj.'properties.StorageProfile'.DataDisks)
   {
-  Get-AzStorageBlobCopyState -Blob $dd.vhd.Uri.Split("/")[4] -Container $dstCN -WaitForComplete
+  Get-AzStorageBlobCopyState -Blob $dd.vhd.Uri.Split("/")[4] -Container $srcCN -WaitForComplete
   }
-
-# Get Destination Storage Account
-$dstStorageAccount = Get-AzStorageAccount -Name $dstStorageAccountName -ResourceGroupName $dstStorageAccountNameRG
 
 # Set Destination VM Hardware Size
 $vm = New-AzVMConfig -VMSize $obj.'properties.hardwareProfile'.vmSize -VMName $vmName
@@ -143,13 +116,13 @@ $vm = New-AzVMConfig -VMSize $obj.'properties.hardwareProfile'.vmSize -VMName $v
 $osDiskName = $vm.Name + "_osdisk_"+$DateStr
 
 # Set OS Uri
-$osVhdUri = "https://"+$dstStorageAccountName+".blob.core.usgovcloudapi.net/" +$dstCN+"/"+$Blob0
+$osVhdUri = "https://"+$dstStorageAccountName+".blob.core.usgovcloudapi.net/" +$srcCN+"/"+$Blob0
 
 # Create OS Disk Config
-$osdiskConfig = New-AzDiskConfig -AccountType $storageType -Location $dstlocation -CreateOption Import -SourceUri $osVhdUri -StorageAccountId $dstStorageAccount.Id
+$osdiskConfig = New-AzDiskConfig -AccountType $storageType -Location $dstlocationName -CreateOption Import -SourceUri $osVhdUri -StorageAccountId $selectdstStorageAccount.Id
 
 # Create OS Managed Disk
-$osDisk = New-AzDisk -DiskName $osDiskName -Disk $osdiskConfig -ResourceGroupName $dstVMRG
+$osDisk = New-AzDisk -DiskName $osDiskName -Disk $osdiskConfig -ResourceGroupName $dstRGName
 
 # Attach OS Managed Disk
 Set-AzVMOSDisk -VM $vm -ManagedDiskId $osDisk.Id -CreateOption "Attach" -Windows
@@ -159,20 +132,19 @@ foreach($dd in $obj.'properties.StorageProfile'.DataDisks)
   {
   $DataDiskvhdName = $dd.vhd.uri.Split("/")[4]
   $DataDiskName = $DataDiskvhdName.Split(".")[0]
-  $DataVhdUri = "https://"+$dstStorageAccountName+".blob.core.usgovcloudapi.net/" +$dstCN+"/"+$dd.vhd.uri.Split("/")[4]
-  $datadiskConfig = New-AzDiskConfig -AccountType $storageType -Location $dstlocation -CreateOption Import -SourceUri $DataVhdUri -StorageAccountId $dstStorageAccount.Id
-  $DataDisk = New-AzDisk -DiskName $DataDiskName -Disk $datadiskConfig -ResourceGroupName $dstVMRG
+  $DataVhdUri = "https://"+$dstStorageAccountName+".blob.core.usgovcloudapi.net/" +$srcCN+"/"+$dd.vhd.uri.Split("/")[4]
+  $datadiskConfig = New-AzDiskConfig -AccountType $storageType -Location $dstlocationName -CreateOption Import -SourceUri $DataVhdUri -StorageAccountId $selectdstStorageAccount.Id
+  $DataDisk = New-AzDisk -DiskName $DataDiskName -Disk $datadiskConfig -ResourceGroupName $dstRGName
   Add-AzVMDataDisk -VM $vm -ManagedDiskId $DataDisk.Id -CreateOption "Attach" -Lun $dd.lun -Caching None
   }
 
 # Create NICs
 foreach($nic in $obj.'properties.networkProfile'.networkInterfaces)
  {
- $OldNicID = $obj.'properties.networkProfile'.networkInterfaces.Id
- $NewNicName = $obj.'properties.networkProfile'.networkInterfaces.Id.Split("/")[8]
- $NewNicID = '/'+$obj.'properties.networkProfile'.networkInterfaces.Id.Split("/")[1]+'/'+$obj.'properties.networkProfile'.networkInterfaces.Id.Split("/")[2]+'/'+$obj.'properties.networkProfile'.networkInterfaces.Id.Split("/")[3]+'/'+$dstVMRG+'/'+$obj.'properties.networkProfile'.networkInterfaces.Id.Split("/")[5]+'/'+$obj.'properties.networkProfile'.networkInterfaces.Id.Split("/")[6]+'/'+$obj.'properties.networkProfile'.networkInterfaces.Id.Split("/")[7]+'/'+$obj.'properties.networkProfile'.networkInterfaces.Id.Split("/")[8]
- $isPrimary = $obj.'properties.networkProfile'.networkInterfaces.'properties.primary'
- New-AzNetworkInterface -ResourceGroupName $dstVMRG -Location $dstlocation -Name $NewNicName -SubnetId $dstVNet.Subnets[0].Id
+  $NewNicName = $nic.Id.Split("/")[8]
+ $NewNicID = '/'+$nic.Id.Split("/")[1]+'/'+$nic.Id.Split("/")[2]+'/'+$nic.Id.Split("/")[3]+'/'+$dstRGName+'/'+$nic.Id.Split("/")[5]+'/'+$nic.Id.Split("/")[6]+'/'+$nic.Id.Split("/")[7]+'/'+$nic.Id.Split("/")[8]
+ $isPrimary = $nic.'properties.primary'
+ New-AzNetworkInterface -ResourceGroupName $dstRGName -Location $dstlocationName -Name $NewNicName -SubnetId $dstVNet.Subnets[0].Id
      IF ($isPrimary -eq 'True'){
      $vm = Add-AzVMNetworkInterface -VM $vm -Id $NewNicID -Primary
      }
@@ -181,11 +153,4 @@ foreach($nic in $obj.'properties.networkProfile'.networkInterfaces)
 
 # Create VM
 Set-AzVMBootDiagnostic -Disable -VM $vm
-New-AzVM -ResourceGroupName $dstVMRG -Location $dstlocation -VM $vm
-
-# Cleanup
-# Remove Restore Storage Account
-Remove-AzStorageAccount -ResourceGroupName $rstStorageAccountNameRG -Name $rstStorageAccountName -Force
-
-# Remove Destination Storage Account
-Remove-AzStorageAccount -ResourceGroupName $dstStorageAccountNameRG -Name $dstStorageAccountName -Force
+New-AzVM -ResourceGroupName $dstRGName -Location $dstlocationName -VM $vm
