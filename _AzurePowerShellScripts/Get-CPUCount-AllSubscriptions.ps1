1f435bffc4a735433a57466e4e675a978f5f10de
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-09-10 14:33:24 -0400

    Finalize Master

--- _AzurePowerShellScripts/Get-CPUCount-AllSubscriptions.ps1
+++ _AzurePowerShellScripts/Get-CPUCount-AllSubscriptions.ps1
@@ -1,6 +1,6 @@
 ﻿$Family = Read-Host "Please Enter the Family Sku Name"
 $Exclude = Read-Host "Please Enter Family Exclusion"
-$CombinedView = @()
+$SubscriptionLocationView = @()
 
 # Select Subscription
 $Subscriptions = Get-AzSubscription -ErrorAction 0
@@ -11,28 +11,27 @@ $AllVMsInSub = Get-azvm -ErrorAction 0 | Where-Object {$_.HardwareProfile.VmSize
 
 $Locations = Get-AzLocation -ErrorAction 0
     foreach ($Location in $Locations){
-    $TotalCPUCount = @()
 
     # Get All VM's with specific Family Type
     $LocationName = $Location.Location
     $VMs = $AllVMsInSub | Where-Object {$_.Location -like $LocationName}
         foreach ($VM in $VMs ){
         $VMSize = (Get-AzVM -Name $VM.Name).HardwareProfile.VmSize
+
         $VMCoreCount = (Get-AzVMSize -VMName $VM.Name -ResourceGroupName $vm.ResourceGroupName -ErrorAction 0 | Where-Object {$_.Name -like $VMSize}).NumberOfCores
-
-        $TotalCPUCount += $VMCoreCount
-
-        $CombinedCPUCount = $TotalCPUCount | Measure-Object -Sum
+        $TotalVMCoreCount = @()
+        $TotalVMCoreCount += $VMCoreCount
+        $LocationCoreCount = $TotalVMCoreCount | Measure-Object -Sum
 
         $obj = new-object psobject -InformationAction SilentlyContinue -Property @{
-            "Total $SubscriptionName $LocationName $Family CPU Count" = $CombinedCPUCount.Sum
+            "Total $SubscriptionName $LocationName $Family CPU Count" = $LocationCoreCount.Sum
                             }
         }
-        $CombinedView += $obj | Format-List "Total $SubscriptionName $LocationName $Family CPU Count"
-}
+        $SubscriptionLocationView += $obj | Format-List "Total $SubscriptionName $LocationName $Family CPU Count"
+    }
 }
 # Clean Up View/Remove File
-$CombinedView | Out-File "C:\Temp\CPUCount.txt"
+$SubscriptionLocationView | Out-File "C:\Temp\CPUCount.txt"
 $file = "C:\Temp\CPUCount.txt"
 $file = Get-Content -Path "C:\Temp\CPUCount.txt"
 $file | ForEach-Object {$_.TrimEnd()} | Where-Object {$_.trim()}
